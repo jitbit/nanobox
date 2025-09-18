6705938c6f6da4fd2e43288bc917a845827c4057
Alex Yumashev <jitbit+github@gmail.com>
2025-09-19 01:13:10 +0300

    Performance improvements, refactor * cache regex * cache DOM element vars * no id-searches * "const" where possible * text-decoration:none for close link

--- nanobox.js
+++ nanobox.js
@@ -1,18 +1,22 @@
 //nanobox - lightest possible lightbox (c) jitbit, Alex Yumashev
 
-"use strict";
 const Nanobox = new (function () {
-	let div = document.createElement('div');
+	const imgRx = /\.(jpg|gif|png|webp|jpeg)$/i;
+	const div = document.createElement('div');
 	div.innerHTML = `<div style='position:fixed;display:none;background:#111111bb;width:100%;height:100%;top:0;left:0;z-index:10000;backdrop-filter:blur(10px)'>
-		<a href="javascript:;" style='position:fixed;right:10px;top:10px;font-size:2em;color:#fff'>×</a>
-		<img id='imgNanobox' style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);max-height:80%' />
-		<iframe id='iframeNanobox' style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);width:80%;height:80%'></iframe>
+		<a href="javascript:;" style='position:fixed;right:10px;top:10px;font-size:2em;color:#fff;text-decoration:none'>×</a>
+		<img style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);max-height:80%' />
+		<iframe style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);width:80%;height:80%'></iframe>
 	</div>`;
-	let overlay = div.firstChild;
+	const overlay = div.firstElementChild;
+	const img = overlay.firstElementChild.nextElementSibling;
+	const ifr = img.nextElementSibling;
+
 	document.body.appendChild(overlay);
-	const closeOverlay = () => {
+
+	function closeOverlay() {
 		overlay.style.display = 'none';
-		_id('iframeNanobox').src = 'about:blank'; //stop YT
+		ifr.src = 'about:blank'; //stop YT
 	};
 	overlay.addEventListener('click', closeOverlay);
 	document.addEventListener('keydown', (e) => {
@@ -20,18 +24,16 @@ const Nanobox = new (function () {
 	});
 
 	this.attach = (selector) => {
-		document.querySelectorAll(selector).forEach(anchor => {
-			let el = /\.(jpg|gif|png|webp|jpeg)/gi.test(anchor.href) ? _id("imgNanobox") : _id("iframeNanobox");
+		document.querySelectorAll(selector).forEach(link => {
+			let el = imgRx.test(link.href) ? img : ifr;
 
-			anchor.addEventListener("click", (e) => {
+			link.addEventListener("click", (e) => {
 				e.preventDefault();
-				_id("imgNanobox").style.display = _id("iframeNanobox").style.display = 'none';
+				img.style.display = ifr.style.display = 'none';
 				el.style.display = '';
-				el.src = anchor.href;
+				el.src = link.href;
 				overlay.style.display = '';
 			});
 		});
 	}
-
-	function _id(id) { return document.getElementById(id); }
 });
