bc2759075399e1f212e0c57a72d108b8a26fb918
Alex Yumashev <jitbit+github@gmail.com>
2025-09-19 08:09:06 +0300

    Border-radius

--- nanobox.js
+++ nanobox.js
@@ -4,9 +4,9 @@ const Nanobox = new (function () {
 	const imgRx = /\.(jpg|gif|png|webp|jpeg)$/i;
 	const div = document.createElement('div');
 	div.innerHTML = `<div style='position:fixed;display:none;background:#111111bb;width:100%;height:100%;top:0;left:0;z-index:10000;backdrop-filter:blur(10px)'>
-		<a href="javascript:;" style='position:fixed;right:10px;top:10px;font-size:2em;color:#fff;text-decoration:none'>×</a>
-		<img style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);max-height:80%' />
-		<iframe style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);width:80%;height:80%'></iframe>
+		<a href="javascript:;" style='position:fixed;right:10px;top:10px;font-size:3em;color:#fff;text-decoration:none'>×</a>
+		<img style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);max-height:80%;border-radius:8px' />
+		<iframe style='display:none;position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);width:80%;height:80%;border-radius:8px;border:none'></iframe>
 	</div>`;
 	const overlay = div.firstElementChild;
 	const img = overlay.firstElementChild.nextElementSibling;
